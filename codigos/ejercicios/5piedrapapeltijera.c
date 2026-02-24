f27935130ea627f8a2e95dda968b22ad89437342
jbarrosmi <jbarrosmi@gmail.com>
2026-02-24 02:02:45 +0100

    se comprueba funcionamiento de todos los archivos después de cambiar json

--- codigos/ejercicios/5piedrapapeltijera.c
+++ codigos/ejercicios/5piedrapapeltijera.c
@@ -14,47 +14,41 @@ int main()
 inicio:
     printf("Bienvenido a Piedra, Papel o Tijera.\n");
     printf("Elige una opcion:\n");
-    printf("1. Piedra.\n2. Papel.\n3. Tijera.\n");
+    printf("1. Piedra.\n2. Papel.\n3. Tijera.\n4. Salir.\n");
     int selec;
     scanf("%d", &selec);
     
     int comp = rand() % 3+1;
-    if(selec==1 && comp==1)
-    {system("cls");
+    if(selec==1 && comp==1){
     printf("Empate. Piedra vs Piedra\n");
     goto inicio;}
-    else if(selec==1 && comp==2)
-    {system("cls");
+    else if(selec==1 && comp==2){
     printf("Pierdes. Piedra vs Papel.\n");
     goto inicio;}
-    else if(selec==1 && comp==3)
-    {system("cls");
+    else if(selec==1 && comp==3){
     printf("Ganas. Piedra vs Tijera.\n");
     goto inicio;}
-    else if(selec==2 && comp==1)
-    {system("cls");
+    else if(selec==2 && comp==1){
     printf("Ganas. Papel vs Piedra\n");
     goto inicio;}
-    else if(selec==2 && comp==2)
-    {system ("cls");
+    else if(selec==2 && comp==2){
     printf("Empate. Papel vs Papel.\n");
     goto inicio;}
-    else if (selec==2 && comp==3)
-    {system("cls");
+    else if (selec==2 && comp==3){
     printf("Pierdes. Papel vs Tijera\n");
     goto inicio;}
-    else if (selec==3 && comp==1)
-    {system("cls");
+    else if (selec==3 && comp==1){
     printf("Pierdes. Tijera vs Piedra.\n");
     goto inicio;}
-    else if (selec==3 && comp==2)
-    {system("cls");
+    else if (selec==3 && comp==2){
     printf("Ganas. Tijera vs Papel.\n");
     goto inicio;}
-    else if (selec==3 && comp==3)
-    {system("cls");
+    else if (selec==3 && comp==3){
     printf("Empate. Tijera vs Tijera.\n");
     goto inicio;}
+    else if (selec==4){
+        return 0;
+    }
 
 
 getchar();
